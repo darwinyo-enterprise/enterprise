3343c891048ee6ab92c92ddea1b7b180cedf7222
Darwin Yo <darwinyo@enterprise.com>
2018-04-13 10:45:38 +0700

    Product, Manufacturer, Category Module Added

--- js/enterprise.frontend.workspace/apps/commerce.admin-app/src/app/catalog/catalog.module.ts
+++ js/enterprise.frontend.workspace/apps/commerce.admin-app/src/app/catalog/catalog.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductModule } from '../product/product.module';
+import { CategoryModule } from '../category/category.module';
+import { ManufacturerModule } from '../manufacturer/manufacturer.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    ProductModule,
+    CategoryModule,
+    ManufacturerModule
   ],
   declarations: []
 })
