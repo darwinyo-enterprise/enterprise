0e805dabf846540d4ad20004a27a76dd5528cc86
Darwin Yo <darwinyo@enterprise.com>
2018-04-13 10:49:14 +0700

    Product, Manufacturer, Category Module Re Organize Folder Structure

--- js/enterprise.frontend.workspace/apps/commerce.admin-app/src/app/catalog/catalog.module.ts
+++ js/enterprise.frontend.workspace/apps/commerce.admin-app/src/app/catalog/catalog.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProductModule } from '../product/product.module';
-import { CategoryModule } from '../category/category.module';
-import { ManufacturerModule } from '../manufacturer/manufacturer.module';
+import { ProductModule } from './product/product.module';
+import { CategoryModule } from './category/category.module';
+import { ManufacturerModule } from './manufacturer/manufacturer.module';
 
 @NgModule({
   imports: [
