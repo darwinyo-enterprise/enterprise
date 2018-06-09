f087f6bfebb2ebe09abf5d83a7fe633354a3d4a6
Darwin Yo <34813094+darwinyo-enterprise@users.noreply.github.com>
2018-06-09 10:36:02 -0700

    Update build.sh

--- script/dotnet/commerce/build.sh
+++ script/dotnet/commerce/build.sh
@@ -1,4 +1,11 @@
-ls
-cd ./net
+cd ./net/tests/Enterprise.Commerce.Tests
 dotnet restore
 dotnet build 
+
+dotnet test
+
+cd ../Enterprise.Library.EventBus.tests
+dotnet restore
+dotnet build 
+
+dotnet test
