a5d0c6b1b6c2b21b40ac508663de858446ac065c
Darwin Yo <darwinyo.enterprise@outlook.com>
2018-06-10 00:43:46 -0700

    try docker compose in travis

---
+++ script/dotnet/commerce/integration-test.sh
@@ -0,0 +1,9 @@
+docker version
+docker-compose version
+
+# cd ../integration-tests/Enterprise.Commerce.IntegrationTests
+# dotnet restore
+# dotnet build 
+
+# dotnet test
+
--- script/dotnet/commerce/test.sh
+++ script/dotnet/commerce/test.sh
@@ -1,5 +1,11 @@
-cd ./tests/Enterprise.Commerce.tests
+cd ./net/tests/Enterprise.Commerce.Tests
+dotnet restore
+dotnet build 
+
 dotnet test
 
 cd ../Enterprise.Library.EventBus.tests
+dotnet restore
+dotnet build 
+
 dotnet test
