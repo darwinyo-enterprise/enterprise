7bbfedf6163f7e36c3350112522e074f8fa51643
Darwin Yo <darwinyo.enterprise@outlook.com>
2018-06-12 16:12:45 -0700

    add local font asset to front end

---
+++ js/enterprise-nx6-workspace/apps/commerce-admin-app/src/main.server.js
@@ -0,0 +1,1 @@
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0});var app_server_module_1=require("./app/app.server.module");exports.AppServerModule=app_server_module_1.AppServerModule;
