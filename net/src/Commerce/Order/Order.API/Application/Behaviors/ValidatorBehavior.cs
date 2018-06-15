af080c781878edf290be744105a4efead49f7c01
Darwin Yo <darwinyo.enterprise@outlook.com>
2018-06-15 15:36:29 -0700

    fix db name

--- net/src/Commerce/Order/Order.API/Application/Behaviors/ValidatorBehavior.cs
+++ net/src/Commerce/Order/Order.API/Application/Behaviors/ValidatorBehavior.cs
@@ -10,7 +10,11 @@ namespace Order.API.Application.Behaviors
     public class ValidatorBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
     {
         private readonly IValidator<TRequest>[] _validators;
-        public ValidatorBehavior(IValidator<TRequest>[] validators) => _validators = validators;
+
+        public ValidatorBehavior(IValidator<TRequest>[] validators)
+        {
+            _validators = validators;
+        }
 
         public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken,
             RequestHandlerDelegate<TResponse> next)
@@ -22,11 +26,9 @@ namespace Order.API.Application.Behaviors
                 .ToList();
 
             if (failures.Any())
-            {
                 throw new OrderingDomainException(
                     $"Command Validation Errors for type {typeof(TRequest).Name}",
                     new ValidationException("Validation exception", failures));
-            }
 
             var response = await next();
             return response;
